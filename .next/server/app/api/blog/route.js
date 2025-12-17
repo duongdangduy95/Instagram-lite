58a8cd8b977e6dfde584da52c7e7a2afe3a5b4d9
duongdangduy95 <duy.dd225185@sis.hust.edu.vn>
2025-12-17 18:52:45 +0700

    Remove .next from repo

--- .next/server/app/api/blog/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/blog/route.js")
-R.c("server/chunks/e1940_next_4bc8b6c2._.js")
-R.c("server/chunks/[root-of-the-server]__df88ccfe._.js")
-R.m("[project]/Desktop/Instagram-lite/.next-internal/server/app/api/blog/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/Desktop/Instagram-lite/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/Instagram-lite/app/api/blog/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/Desktop/Instagram-lite/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/Instagram-lite/app/api/blog/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
